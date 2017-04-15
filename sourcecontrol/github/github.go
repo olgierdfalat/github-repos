64987470525702a0386340540f667bd86f1efb39
Michael Lockwood <mlockwood.1989@gmail.com>
2017-04-15 16:55:07 +0100

    Mocked data in github.go

--- sourcecontrol/github/github.go
+++ sourcecontrol/github/github.go
@@ -1,14 +1,25 @@
 package github
 
-import "bitbucket.org/michaellockwood/github-repos/sourcecontrol"
+import (
+	"bitbucket.org/michaellockwood/github-repos/sourcecontrol"
+	"time"
+)
 
 type GitHubGateway struct {
 }
 
 func (gateway GitHubGateway) GetRepositories(query string, total int) []sourcecontrol.Repository {
-	return make([]sourcecontrol.Repository, 0)
+	return []sourcecontrol.Repository {
+		{"Go", "Go", "www.go.com", time.Now(), time.Now(), nil},
+	}
 }
 
 func (gateway GitHubGateway) GetCommits(owner string, repoName string, total int) []sourcecontrol.Commit {
-	return make([]sourcecontrol.Commit, 0)
+	return []sourcecontrol.Commit {
+		{ "Google", "54544545", "Commit num 1", time.Now() },
+		{ "Google", "gfdg43", "Commit num 2", time.Now() },
+		{ "Google", "543ghxf", "Commit num 3", time.Now() },
+		{ "Google", "868bc", "Commit num 4", time.Now() },
+		{ "Google", "dfgdgf5", "Commit num 5", time.Now() },
+	}
 }
