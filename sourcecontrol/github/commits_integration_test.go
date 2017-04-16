4c857e122356d2a5e9056528f5305d5f0e9f6623
Michael Lockwood <mlockwood.1989@gmail.com>
2017-04-16 14:19:22 +0100

    Added integration tests for commits.

---
+++ sourcecontrol/github/commits_integration_test.go
@@ -0,0 +1,57 @@
+package github_test
+
+import (
+	"testing"
+	"bitbucket.org/michaellockwood/github-repos/sourcecontrol/github"
+	"time"
+)
+
+func TestGitHubGateway_GetCommits_ReturnsCorrectAmountOfCommits(t *testing.T) {
+	cases := []struct {
+		count int
+	}{
+		{ 10 },
+		{ 5 },
+	}
+
+	for _, c := range cases {
+		gateway := github.GitHubGateway{}
+		commits, err := gateway.GetCommits("golang", "go", c.count)
+
+		if err != nil {
+			t.Fatalf("GetRepositories failed with error: %v", err)
+		}
+
+		if len(commits) != c.count {
+			t.Fatalf("Expected repo count: %v, Actual: %v", c.count, len(commits))
+		}
+	}
+}
+
+func TestGitHubGateway_GetCommits_ReturnsDomainCommits(t *testing.T) {
+	gateway := github.GitHubGateway{}
+	commits, err := gateway.GetCommits("golang", "go", 5)
+
+	if err != nil {
+		t.Fatalf("GetRepositories failed with error: %v", err)
+	}
+
+	for _, c := range commits {
+		if c.Author == "" {
+			t.Fatal("Author was empty.")
+		}
+
+		if c.SHA == "" {
+			t.Fatal("SHA was empty.")
+		}
+
+		if c.Message == "" {
+			t.Fatal("Message was empty.")
+		}
+
+		zeroTime := time.Time{}
+		if c.Date == zeroTime {
+			t.Fatal("Date was zero time.")
+		}
+	}
+}
