90dc41e52e84c5fbfb36090a2c87aaaa7bb04cec
Michael Lockwood <mlockwood.1989@gmail.com>
2017-04-16 14:27:35 +0100

    Implemented commits logic.

--- sourcecontrol/github/commits.go
+++ sourcecontrol/github/commits.go
@@ -3,16 +3,48 @@ package github
 import (
 	"bitbucket.org/michaellockwood/github-repos/sourcecontrol"
 	"time"
+	"fmt"
 )
 
 const CommitPathFormat string = "/repos/%s/%s/commits?per_page=%v"
 
+type author struct {
+	Name string
+	Date time.Time
+}
+
+type commit struct {
+	URL string
+	Author author
+	Message string
+}
+
+type commitResponse struct {
+	SHA string
+	Commit commit
+}
+
 func (gateway GitHubGateway) GetCommits(owner string, repoName string, total int) ([]sourcecontrol.Commit, error) {
-	return []sourcecontrol.Commit {
-		{ "Google", "54544545", "Commit num 1", time.Now() },
-		{ "Google", "gfdg43", "Commit num 2", time.Now() },
-		{ "Google", "543ghxf", "Commit num 3", time.Now() },
-		{ "Google", "868bc", "Commit num 4", time.Now() },
-		{ "Google", "dfgdgf5", "Commit num 5", time.Now() },
-	}, nil
+	commitResponses := []commitResponse{}
+	err := getAndUnmarshall(fmt.Sprintf(CommitPathFormat, owner, repoName, total), &commitResponses)
+
+	if err != nil {
+		return nil, err
+	}
+
+	commits := make([]sourcecontrol.Commit, len(commitResponses))
+	for i, c := range commitResponses {
+		commits[i] = c.mapToDomain()
+	}
+
+	return commits, nil
+}
+
+func (commitResponse *commitResponse) mapToDomain() sourcecontrol.Commit {
+	return sourcecontrol.Commit{
+		Author: commitResponse.Commit.Author.Name,
+		Date: commitResponse.Commit.Author.Date,
+		Message: commitResponse.Commit.Message,
+		SHA: commitResponse.SHA,
+	}
 }
